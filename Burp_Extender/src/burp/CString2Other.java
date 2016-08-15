18f16e51ac072cd47f44563ee542a61df0f5d0cd
bit4 <bit4>
2016-08-15 17:35:55 +0800

    update

--- Burp_Extender/src/burp/CString2Other.java
+++ Burp_Extender/src/burp/CString2Other.java
@@ -47,7 +47,7 @@ public class CString2Other {
     
     
 	public static void main(String args[]) throws Exception {
-		String test = "{\"order_id\":\"2011608112040003175\",\"pay_info\":{\"method\":\"GET\",\"params\":{\"amt\":\"1.00\",\"body\":\"综合意外险\",\"expiry_time\":\"1440\",\"merchant_code\":\"1512000401\",\"notify_info\":\"{}\",\"notify_url\":\"http://14.29.68.179:8092/receive/order/state/zhongan_pay\",\"order_info\":\"xx\",\"order_type\":\"insurance\",\"out_trade_no\":\"2011608112040003175\",\"pay_channel\":\"alipay^wxpay\",\"request_charset\":\"UTF-8\",\"return_url\":\"https://jr.meizu.com/h5/html/insurance/success.html\",\"sign\":\"7f44a0d33abb99309ffd73ea21bba840\",\"sign_type\":\"MD5\",\"src_type\":\"mobile\",\"subject\":\"综合意外险\"},\"uri\":\"http://cashier.itest.zhongan.com/za-cashier-web/gateway.do\"}}"; 
+		String test = "{\"order_id\":\"2011608112040003175\",\"pay_info\":{\"method\":\"GET\",\"params\":{\"amt\":\"1.00\",\"body\":\"综合意外险\",\"expiry_time\":\"1440\",\"merchant_code\":\"1512000401\",\"notify_info\":\"{}\",\"notify_url\":\"http://14.29.00.179:8092\",\"order_info\":\"xx\",\"order_type\":\"insurance\",\"out_trade_no\":\"2011608112040003175\",\"pay_channel\":\"alipay^wxpay\",\"request_charset\":\"UTF-8\",\"return_url\":\"https://jr.xxx.com/\",\"sign\":\"xxxx\",\"sign_type\":\"MD5\",\"src_type\":\"mobile\",\"subject\":\"综合意外险\"},\"uri\":\"http://cashier.itestxxxx.xx.com/za-cashier-web/gateway.do\"}}"; 
 		JSONObject jO = JSONString2JSONObj(test);
 		//System.out.println(jO);
 		Map<String,Object> map = JSONOString2Map(test);
